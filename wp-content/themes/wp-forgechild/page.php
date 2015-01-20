c454c6d7564239324e7d71a8e4b22cef2adef884
Primary Developer <dev@culturesofknowledge.sers.ox.ac.uk>
2015-01-20 17:00:37 +0000

    Add content section to wordpress pages

--- wp-content/themes/wp-forgechild/page.php
+++ wp-content/themes/wp-forgechild/page.php
@@ -16,6 +16,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 	 <div class="row">
       <div class="large-3 columns">
+<?php 
+	$custom_fields = get_post_custom(); 
+  echo "<!-- ";
+	var_dump($custom_fields);
+	echo " -->";
+	if( array_key_exists( "catalogue_contents", $custom_fields ) ) {
+		echo '<ul class="side-nav floating-menu">';
+		foreach( $custom_fields["catalogue_contents"] as $menuitem ) {
+			$items = explode("|", $menuitem );
+			echo '<li><a href="#' . $items[0] . '">' . $items[1] . '</a></li>';
+    }
+		echo "</ul>";
+	}
+?>
         <br>
       </div>
 		<div id="content" class="site-content large-9 columns" role="main">
