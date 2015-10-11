50d723092f3da93b3fa7523f166138de7a81bd56
EMLO Server <EMLO.Server@bodleian.ox.ac.uk>
2015-10-11 22:06:06 +0100

    Main omeka files for baxter exhibition

---
+++ baxter/plugins/HistoryLog/controllers/LogController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * HistoryLog
+ *
+ * @copyright Copyright 2014 UCSC Library Digital Initiatives
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
+ */
+
+/**
+ * The HistoryLog log controller class.
+ *
+ * @package HistoryLog
+ */
+class HistoryLog_LogController extends Omeka_Controller_AbstractActionController
+{    
+ 
+  /**
+   *Set up the view for full item reports
+   *
+   *@return void
+   */
+  public function showAction()
+  {
+
+    $flashMessenger = $this->_helper->FlashMessenger;
+    $item = $this->_getParam('item');
+    if(empty($item))
+      $flashMessenger->addMessage("Item not selected.","error");
+    $this->view->itemID = $item;
+  }
+
+}
