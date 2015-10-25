eb70b04324863d84dce15be367fbd3c7e8dab3ab
EMLO Server <EMLO.Server@bodleian.ox.ac.uk>
2015-10-25 21:24:39 +0000

    Fix site main title display

--- baxter/themes/firefly/common/header.php
+++ baxter/themes/firefly/common/header.php
@@ -43,7 +43,7 @@
 				<div id="header-wrap">
         <header role="banner">
             <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
-            <div id="site-title"><a href="http://emlo.bodleian.ox.ac.uk/exhibition/baxter/">Baxter</a></div>
+            <div id="site-title"><a href="/exhibition/baxter/"><?php echo option("site_title")?></a></div>
 
             <div id="search-container" role="search">
                 <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>
