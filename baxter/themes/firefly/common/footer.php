feb50df29faa69d3b7c91e7f1fa4cbadada10afa
EMLO Server <EMLO.Server@bodleian.ox.ac.uk>
2015-11-04 22:20:29 +0000

    Add google analytics

--- baxter/themes/firefly/common/footer.php
+++ baxter/themes/firefly/common/footer.php
@@ -38,11 +38,22 @@
 
 <script type="text/javascript">
     jQuery(document).ready(function(){
+        Berlin.dropDown();
         Omeka.showAdvancedForm();
         Omeka.skipNav();
         Omeka.megaMenu();
-        Berlin.dropDown();
     });
+
+
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-23877246-5']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
 </script>
 
 </body>
