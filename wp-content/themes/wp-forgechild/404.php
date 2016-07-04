f4c12850a7b2099e2ccb7e41a5f8a7f21fa854a8
EMLO Server <EMLO.Server@bodleian.ox.ac.uk>
2016-07-04 15:37:35 +0100

    Remove stage VM references

--- wp-content/themes/wp-forgechild/404.php
+++ wp-content/themes/wp-forgechild/404.php
@@ -28,17 +28,17 @@ get_header(); ?>
 					<p><?php _e( 'The page you were looking for might not be here. It could have been moved, deleted or maybe the URL you typed or the link you clicked was incorrect in some way.', 'wpforge' ); ?></p>
 
 	                <br>
-	                <p style="font-size:x-small;"><img src="/images/EMLO_404.png" style="padding:10px; background:#fff; border:1px solid #999;"/><br>
+	                <p style="font-size:x-small;"><img src="http://emlo.bodleian.ox.ac.uk/images/EMLO_404.png" style="padding:10px; background:#fff; border:1px solid #999;"/><br>
 	                <a href="http://www.rijksmuseum.nl/collectie/RP-P-OB-7367/de-alchemist" target="_blank">Image Source: Rijksmuseum, Amsterdam. CC BY 3.0.</a></p>
 	                <br>
-	                <p>You can try the Basic and Intermediate searches on our <a href="http://emlo-stage.bodleian.ox.ac.uk/">Homepage</a>, try an <a href="http://emlo-stage.bodleian.ox.ac.uk/advanced">Advanced Search</a>, or go to <a href="http://emlo-stage.bodleian.ox.ac.uk/browse/people">Browse</a></p>
+	                <p>You can try the Basic and Intermediate searches on our <a href="http://emlo.bodleian.ox.ac.uk/">Homepage</a>, try an <a href="http://emlo.bodleian.ox.ac.uk/advanced">Advanced Search</a>, or go to <a href="http://emlo.bodleian.ox.ac.uk/browse/people">Browse</a></p>
 	                <br>
-	                <p>You can also check our <a href="http://emlo-stage.bodleian.ox.ac.uk/humans.txt" target="_blank">humans.txt</a> to discover who participated in this project!</p>
+	                <p>You can also check our <a href="http://emlo.bodleian.ox.ac.uk/humans.txt" target="_blank">humans.txt</a> to discover who participated in this project!</p>
 	                <br>
  
 					<h5><b><?php _e( 'Try Again?', 'wpforge' ); ?></b></h5>
 					<p><?php _e( 'I know this didn&rsquo;t work before but you may want to try searching again, only this time keep your fingers crossed!', 'wpforge' ); ?></p>
-						<form  action="http://emlo-stage.bodleian.ox.ac.uk/forms/quick" method="get">
+						<form  action="http://emlo.bodleian.ox.ac.uk/forms/quick" method="get">
 						<div class="row collapse">
 						<div class="large-5 medium-5 small-12 columns">
 						<input type="text" value="<?php echo esc_attr($s); ?>" id="s" name="s" /> 
