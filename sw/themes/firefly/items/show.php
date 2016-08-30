f4f88d02c69f5bc37840d8332571e174ffa16d0a
EMLO Server <EMLO.Server@bodleian.ox.ac.uk>
2016-08-30 13:41:20 +0100

    Remove previous and next links on items!

--- sw/themes/firefly/items/show.php
+++ sw/themes/firefly/items/show.php
@@ -76,10 +76,10 @@
        <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
 
-    <ul class="item-pagination navigation">
+    <!-- <ul class="item-pagination navigation">
         <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
         <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-    </ul>
+    </ul> -->
 
 </div> <!-- End of Primary. -->
 
