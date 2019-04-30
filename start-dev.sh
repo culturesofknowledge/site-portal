cbacc01c0dc6ec8482c11de75fb0bad798d42024
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2019-04-30 14:59:40 +0100

    Easy start

---
+++ start-dev.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo
+echo "Setting up DEVELOPMENT copy of EMLO-Portal..."
+echo
+echo
+
+# Start (and build if necessary)
+docker-compose --file docker-compose-dev.yaml up -d --build
+sleep 5
+docker-compose --file docker-compose-dev.yaml logs --tail="10"
+
+# Output status
+echo
+echo "Container status:"
+docker-compose --file docker-compose-dev.yaml ps
+
+# Tail all the output (Ctrl-c to stop)
+echo
+echo "Tailing logs (Ctrl-c to stop logs):"
+docker-compose --file docker-compose-dev.yaml logs -f --tail="0"
---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo
+echo "Setting up PRODUCTION copy of EMLO-Portal..."
+echo
+echo
+
+# Build, run, in background.
+docker-compose up -d --build --remove-orphans
+sleep 5
+docker-compose ps
