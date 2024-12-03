346b05525132b8960b364c49cc68b20b5f795719
John Pybus <john.pybus@oerc.ox.ac.uk>
2024-12-03 16:28:58 +0000

    Adjustment for newer docker compose

--- start-dev.sh
+++ start-dev.sh
@@ -6,16 +6,16 @@ echo
 echo
 
 # Start (and build if necessary)
-docker-compose --file docker-compose-dev.yaml up -d --build
+docker compose --file docker-compose-dev.yaml up -d --build
 sleep 5
-docker-compose --file docker-compose-dev.yaml logs --tail="10"
+docker compose --file docker-compose-dev.yaml logs --tail="10"
 
 # Output status
 echo
 echo "Container status:"
-docker-compose --file docker-compose-dev.yaml ps
+docker compose --file docker-compose-dev.yaml ps
 
 # Tail all the output (Ctrl-c to stop)
 echo
 echo "Tailing logs (Ctrl-c to stop logs):"
-docker-compose --file docker-compose-dev.yaml logs -f --tail="0"
+docker compose --file docker-compose-dev.yaml logs -f --tail="0"
--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ echo
 echo
 
 # Build, run, in background.
-docker-compose up -d --build --remove-orphans
+docker compose up -d --build --remove-orphans
 sleep 5
-docker-compose ps
+docker compose ps
